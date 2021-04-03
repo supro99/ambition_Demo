1b8fd6f5dd49aed92be0be3b79ba892587884b5d
Supriya Patil <supriyapatil1171@gmail.com>
2021-04-03 19:45:45 +0530

    Database Configuration changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'database_url':'mongodb://admin:passadmin@localhost/admin',
+    'database_url':'mongodb://localhost:27017',
     'port':'3000',
     'ambition_db': 'ambition-db',
     'userCollection': 'users',
